484771499fc159ce63f289793f892a4967e1a2ed
efecan-circlelabs <148798298+efecan-circlelabs@users.noreply.github.com>
2023-11-30 16:35:12 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
         "charset_normalizer >= 3.0.1; python_version < '3.7'",
         "charset_normalizer >= 3.2.0; python_version >= '3.7'",
         "courlan >= 0.9.5",
-        "htmldate >= 1.6.0",
+        "git+https://github.com/efecan-circlelabs/htmldate@master[speed]",
         "justext >= 3.0.0",
         "lxml >= 4.9.3",
         "urllib3 >= 1.26, < 2; python_version < '3.7'",
